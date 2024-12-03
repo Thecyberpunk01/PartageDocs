3a0afd802108351088e41bac36f644d79351eb8b
Thecyberpunk01 <160755250+Thecyberpunk01@users.noreply.github.com>
2024-12-03 23:12:29 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,28 +1,56 @@
-<?php
-
-$servername = "sql103.infinityfree.com";
-
-$username = "if0_37212495";
-
-$password = "Aqd29Ur7zD";
-
-$dbname = "if0_37212495_inscription_sitetude";
-
-
-
-// Créer la connexion
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-
-// Vérifier la connexion
-
-if ($conn->connect_error) {
-
-    die("La connexion a échoué : " . $conn->connect_error);
-
-}
-
-?>
-
+<?php
+
+
+
+$servername = "sql103.infinityfree.com";
+
+
+
+$username = "";
+
+
+
+$password = "";
+
+
+
+$dbname = "if0_37212495_inscription_sitetude";
+
+
+
+
+
+
+
+// Créer la connexion
+
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+
+
+
+
+
+// Vérifier la connexion
+
+
+
+if ($conn->connect_error) {
+
+
+
+    die("La connexion a échoué : " . $conn->connect_error);
+
+
+
+}
+
+
+
+?>
+
+
+
